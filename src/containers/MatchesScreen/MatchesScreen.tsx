d62a1c6fb9df0734d5908d6c597c22b21ffb6e39
tranducminh <minhscdhpvn@gmail.com>
2020-05-20 00:36:50 +0700

    add navigator

---
+++ src/containers/MatchesScreen/MatchesScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const MatchesScreen = () => {
+  return (
+    <View>
+      <Text />
+    </View>
+  );
+};
+
+export default MatchesScreen;
+
+const styles = StyleSheet.create({});
